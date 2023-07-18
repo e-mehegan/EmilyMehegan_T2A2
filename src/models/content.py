d8a753e5c1d1387bf10c8b574dd6bf599499b1d0
e-mehegan <14220@coderacademy.edu.au>
2023-07-18 17:36:08 +1000

    Create Content Model

---
+++ src/models/content.py
@@ -0,0 +1,29 @@
+from init import db, ma
+from marshmallow import fields
+
+class Content(db.Model):
+    __tablename__ = "content"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    genre = db.Column(db.String)
+    description = db.Column(db.Text)
+    published = db.Column(db.Date)
+    publisher = db.Column(db.String)
+
+
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
+    category_id_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+
+# enter relationships
+
+# enter contentschema
+class ContentSchema(ma.Schema):
+    pass 
+
+    class Meta:
+        fields = ('id', 'title', 'genre', 'description', 'published', 'publisher')
+        ordered = True
+
+content_schema = ContentSchema()
+content_schema = ContentSchema(many=True)
