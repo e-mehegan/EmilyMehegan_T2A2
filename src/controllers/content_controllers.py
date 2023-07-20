c06c2bd2a91e7c9884e1502ef18cb0919346b352
e-mehegan <14220@coderacademy.edu.au>
2023-07-20 11:30:45 +1000

    Add Get All Content Route

--- src/controllers/content_controllers.py
+++ src/controllers/content_controllers.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, request
 from init import db
 from models.content import Content, content_schema, contents_schema
-from models.user import User
 
 content_bp = Blueprint('content', __name__, url_prefix='/content')
 
+@content_bp.route('/')
+def get_all_content():
+    stmt = db.select(Content).order_by(Content.id.desc())
+    contents = db.session.scalars(stmt)
+    return contents_schema.dump(contents)
