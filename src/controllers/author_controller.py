e9eaaca359d693b44ae9fd264457875e689e95ae
e-mehegan <14220@coderacademy.edu.au>
2023-07-22 13:00:22 +1000

    Add routes from author and category

--- src/controllers/author_controller.py
+++ src/controllers/author_controller.py
@@ -5,9 +5,28 @@ from models.user import User
 from models.author import Author, authors_schema, author_schema
 from flask_jwt_extended import get_jwt_identity, jwt_required
 
+def authorise_admin():
+    """
+    Check if the current user is an admin.
+
+    This function checks if the current user, identified by the JWT token,
+    is the admin. It retrieves the user from the database based on
+    the user ID from the JWT token. It returns a boolean value
+    indicating whether the user is an admin.
+
+    Parameters:
+        None.
+
+    Returns:
+        bool: True if the current user is an admin, False otherwise.
+    """
+    user_id = get_jwt_identity()
+    stmt = db.select(User).filter_by(id=user_id)
+    user = db.session.scalar(stmt)
+    return user.is_admin
+
 # Blueprint for author routes
 author_bp = Blueprint('author', __name__, url_prefix='/author')
-
 
 @author_bp.route('/')
 def get_all_authors():
@@ -49,3 +68,113 @@ def get_one_author(id):
         # Return an error message if the input ID is not found
         return {'Error': f'Author not found with the id {id}'}, 404
     
+
+@author_bp.route('/', methods=['POST'])
+@jwt_required()
+def create_author():
+    """
+    Route for creating a new author.
+
+    This function creates a new author by extracting the author data from the request JSON
+    and adding it to the database. Only adminis can create a new author.
+
+    Returns:
+        dict: A dictionary containing the details of the newly created author if successful.
+
+    Raises:
+        403 Forbidden: If the user making the request is not an admin, an error message is returned.
+    """
+    # Check authorization, if not admin return an error
+    json_data = request.get_json()
+    is_admin = authorise_admin()
+    if not is_admin:
+        return {'Error': 'You must be an admin to create a new author.'}, 403
+
+    # Extract author data from the request JSON
+    author = json_data.get('author')
+
+    # Create new author object with provided data
+    authors = Author(
+        author=author
+    )
+    # Add the author to the database and commit the changes
+    db.session.add(authors)
+    db.session.commit()
+    return author_schema.dump(authors), 201
+
+
+@author_bp.route('/<int:id>', methods=['DELETE'])
+@jwt_required()
+def delete_one_author(id):
+    """
+    Route for deleting one author by ID.
+
+    This function allows adminis to delete an author from the database based on the provided ID.
+
+    Args:
+        id (int): The ID of the author to be deleted.
+
+    Returns:
+        dict: A dictionary containing the result of the operation.
+              If the author is successfully deleted, it returns a message confirming the deletion.
+              If the author with the given ID does not exist, it returns an error message with status 404.
+
+    Raises:
+        403 Forbidden: If the user making the request is not an admin, it raises an exception.
+    """
+    # Check authorization, if not admin return an error
+    admin_status = authorise_admin()
+    if not admin_status:
+        return {'Error': 'You must be an admin to delete a author.'}
+
+    stmt = db.select(Author).filter_by(id=id)
+    author = db.session.scalar(stmt)
+
+    if author:
+        db.session.delete(author)
+        db.session.commit()
+        return {'Message': f'Author has been deleted successfully.'}
+    else: 
+        return {'Error': f'Author with the id {id} does not exist.'}, 404 
+    
+
+@author_bp.route('/<int:id>', methods=['PUT', 'PATCH'])
+@jwt_required()
+def update_one_author(id):
+    """
+    Route for updating an author by ID.
+
+    This function allows adminis to update an author's information in the database based on the provided ID.
+
+    Args:
+        id (int): The ID of the author to be updated.
+
+    Returns:
+        dict: A dictionary containing the updated information of the author if successful.
+
+    Raises:
+        403 Forbidden: If the user making the request is not an admin, an error message is returned.
+        404 Not Found: If the author with the given ID does not exist, an error message is returned.
+
+    Note:
+        The function expects the updated author information in the request JSON, and it performs a partial update.
+        If a field is not provided in the request JSON, the existing value in the database for that field will be retained.
+    """
+    json_data = author_schema.load(request.get_json(), partial=True)
+    stmt = db.select(Author).filter_by(id=id)
+    author = db.session.scalar(stmt)
+
+    # Check authorization, if not admin return an error
+    if author:
+        is_admin = authorise_admin()
+        if not is_admin:
+            return {'Error': 'You must be an admin to edit any authors.'}, 403
+
+        # Update author fields if provided, otherwise keep the existing values from database
+        author.author = json_data.get('author', author.author)
+
+        db.session.commit()
+        # Return the updated author as JSON with HTTP status code 200 (OK), if id doesn't exist return error
+        return author_schema.dump(author)
+    else:
+        return {'Error': f'Author with id {id} does not exist.'}, 404
--- src/controllers/category_controller.py
+++ src/controllers/category_controller.py
@@ -5,8 +5,31 @@ from models.user import User
 from models.category import Category, category_schema, categories_schema
 from flask_jwt_extended import get_jwt_identity, jwt_required
 
+
+def authorise_admin():
+    """
+    Check if the current user is an admin.
+
+    This function checks if the current user, identified by the JWT token,
+    is the admin. It retrieves the user from the database based on
+    the user ID from the JWT token. It returns a boolean value
+    indicating whether the user is an admin.
+
+    Parameters:
+        None.
+
+    Returns:
+        bool: True if the current user is an admin, False otherwise.
+    """
+    user_id = get_jwt_identity()
+    stmt = db.select(User).filter_by(id=user_id)
+    user = db.session.scalar(stmt)
+    return user.is_admin
+
+
 # Blueprint for category routes
 category_bp = Blueprint('category', __name__, url_prefix='/category')
+
 
 @category_bp.route('/')
 def get_all_categories():
@@ -24,6 +47,7 @@ def get_all_categories():
     stmt = db.select(Category).order_by(Category.id.desc())
     categories = db.session.scalars(stmt)
     return categories_schema.dump(categories)
+
 
 @category_bp.route('/<int:id>')
 def get_one_category(id):
@@ -48,3 +72,65 @@ def get_one_category(id):
         return {'Error': f'Category not found with the id {id}'}, 404
     
 
+@category_bp.route('/', methods=['POST'])
+@jwt_required()
+def create_category():
+    json_data = request.get_json()
+    is_admin = authorise_admin()
+    if not is_admin:
+        return {'Error': 'You must be an admin to create a new category.'}, 403
+
+    # Extract category data from the request JSON
+    category = json_data.get('category')
+
+    # Create new category object with provided data
+    categories = Category(
+        category=category
+    )
+
+    # Add the category to the database and commit the changes
+    db.session.add(categories)
+    db.session.commit()
+    return category_schema.dump(categories), 201
+
+
+@category_bp.route('/<int:id>', methods=['DELETE'])
+@jwt_required()
+def delete_one_category(id):
+    # Check authorization, if not admin return an error
+    admin_status = authorise_admin()
+    if not admin_status:
+        return {'Error': 'You must be an admin to delete a category.'}
+
+    stmt = db.select(Category).filter_by(id=id)
+    category = db.session.scalar(stmt)
+
+    if category:
+        db.session.delete(category)
+        db.session.commit()
+        return {'Message': f'Category {category} has been deleted successfully.'}
+    else: 
+        return {'Error': f'Category with the id {id} does not exist.'}, 404
+    
+
+@category_bp.route('/<int:id>', methods=['PUT', 'PATCH'])
+@jwt_required()
+def update_one_category(id):
+    json_data = category_schema.load(request.get_json(), partial=True)
+    stmt = db.select(Category).filter_by(id=id)
+    category = db.session.scalar(stmt)
+
+    # Check authorization, if not admin return an error
+    if category:
+        is_admin = authorise_admin()
+        if not is_admin:
+            return {'Error': 'You must be an admin to edit any categories.'}, 403
+
+        # Update category fields if provided, otherwise keep the existing values from database
+        category.category = json_data.get('category', category.category)
+
+        db.session.commit()
+        # Return the updated category as JSON with HTTP status code 200 (OK), if id doesn't exist return error
+        return category_schema.dump(category)
+    else:
+        return {'Error': f'Category with id {id} does not exist.'}, 404
